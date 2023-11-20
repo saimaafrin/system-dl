8feb868eaa6794384f8d0ca6075ae2f857920b94
Pradeep Kumar <pkumar@wm.edu>
2023-11-20 09:16:26 -0500

    removed build files

--- build/CMakeFiles/libgp.so.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libgp.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/libgp.so.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/libgp.so.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for libgp.so.
-# This may be replaced when dependencies are built.
